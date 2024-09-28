173a8133301fc49b3ea90ee88a583584afcc52e6
kanade0404 <melty0404@gmail.com>
2024-09-28 18:16:53 +0900

    :sparkles: Cloud RunをV2に移行してdatabase urlの環境変数をsecret manager経由にする

--- terraform/modules/secret_manager/main.tf
+++ terraform/modules/secret_manager/main.tf
@@ -19,3 +19,12 @@ variable "data" {
   type      = string
   sensitive = true
 }
+output "name" {
+  value = google_secret_manager_secret.secret.name
+}
+output "secret_id" {
+  value = google_secret_manager_secret.secret.secret_id
+}
+output "version" {
+  value = google_secret_manager_secret_version.secret-version.version
+}
